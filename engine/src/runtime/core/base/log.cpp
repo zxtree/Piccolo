a3325a403dfaecf262f185926f55be9f24cbd608
zxtree <zxtree2006@hotmail.com>
2023-03-24 22:56:38 +0800

    log

--- engine/src/runtime/core/base/log.cpp
+++ engine/src/runtime/core/base/log.cpp
@@ -1,12 +1,17 @@
 #include "log.h"
 
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+namespace Piccolo {
+
 std::shared_ptr<spdlog::async_logger> _g_logger_ = nullptr;
 
 void init_log()
 {
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
     console_sink->set_level(spdlog::level::trace);
-    console_sink->set_pattern("[%^%l%$] %v");
+    console_sink->set_pattern("[%H:%M:%S.%e][%^%l%$]%v");
 
     const spdlog::sinks_init_list sink_list = {console_sink};
 
@@ -20,4 +25,6 @@ void init_log()
     _g_logger_->set_level(spdlog::level::trace);
 
     spdlog::register_logger(_g_logger_);
+}
+
 }
--- engine/src/runtime/core/base/log.h
+++ engine/src/runtime/core/base/log.h
@@ -2,31 +2,62 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/async.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
+
+#ifdef SPDLOG_ACTIVE_LEVEL
+    #undef SPDLOG_ACTIVE_LEVEL
+    #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#endif
+#define SPDLOG_DEBUG_ON
+#define SPDLOG_TRACE_ON
+
+namespace Piccolo {
 
 extern std::shared_ptr<spdlog::async_logger> _g_logger_;
 
 extern void init_log();
 
+// copy from spdlog/pattern_formatter-inl.h
+static const char *basename(const char *filename)
+{
+    // if the size is 2 (1 character + null terminator) we can use the more efficient strrchr
+    // the branch will be elided by optimizations
+    if (sizeof(spdlog::details::os::folder_seps) == 2)
+    {
+        const char *rv = std::strrchr(filename, spdlog::details::os::folder_seps[0]);
+        return rv != nullptr ? rv + 1 : filename;
+    }
+    else
+    {
+        const std::reverse_iterator<const char *> begin(filename + std::strlen(filename));
+        const std::reverse_iterator<const char *> end(filename);
+
+        const auto it = std::find_first_of(begin, end, std::begin(spdlog::details::os::folder_seps), std::end(spdlog::details::os::folder_seps) - 1);
+        return it != end ? it.base() : filename;
+    }
+}
+
+
 #define LOGGER() \
-    if(_g_logger_ == nullptr) init_log(); \
-    _g_logger_
+    if(Piccolo::_g_logger_ == nullptr) Piccolo::init_log(); \
+    Piccolo::_g_logger_
+
+#define LOG_FUNC_TAG "[" + std::string(__FUNCTION__) + "@" + std::string(Piccolo::basename(__FILE__)) + ":" + std::to_string(__LINE__) + "] "
 
 #define LOG_DEBUG(...) \
-    LOGGER()->debug("[" + std::string(__FUNCTION__) + "]" + __VA_ARGS__);
+    LOGGER()->debug(LOG_FUNC_TAG + __VA_ARGS__);
 
 #define LOG_INFO(...) \
-    LOGGER()->info("[" + std::string(__FUNCTION__) + "]" + __VA_ARGS__);
+    LOGGER()->info(LOG_FUNC_TAG + __VA_ARGS__);
 
 #define LOG_WARN(...) \
-    LOGGER()->warn("[" + std::string(__FUNCTION__) + "]" + __VA_ARGS__);
+    LOGGER()->warn(LOG_FUNC_TAG + __VA_ARGS__);
 
 #define LOG_ERROR(...) \
-    LOGGER()->error("[" + std::string(__FUNCTION__) + "]" + __VA_ARGS__);
+    LOGGER()->error(LOG_FUNC_TAG + __VA_ARGS__);
 
 #define LOG_SYS_ERR(STR, CODE) \
-    LOGGER()->error("[" + std::string(__FUNCTION__) + "]" + STR + std::system_category().message(CODE));
+    LOGGER()->error(LOG_FUNC_TAG + STR + std::system_category().message(CODE));
 
 #define LOG_FATAL(...) \
-    LOGGER()->critical("[" + std::string(__FUNCTION__) + "]" + __VA_ARGS__);
+    LOGGER()->critical(LOG_FUNC_TAG + __VA_ARGS__);
+}
